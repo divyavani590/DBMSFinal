2a22c656552d89282f3b991b469dcbc01b687b74
Divya Vani Vasarla <vasarlad1@montclair.edu>
2018-12-20 08:53:02 -0500

    order page

--- order.php
+++ order.php
@@ -1,48 +1,34 @@
+<?php include('header.php')?>
 <html>
 <head>
 <title>ICTYA Collections</title>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
 <link rel="stylesheet" href="style.css" />
 </head>
 <body class="item-pages" style="background-image:url('images\winter.jpeg')">
-<h1> <font face="courier" color="#C70039">ICTYA Collections </font></h1>
-<ul>
-  <li><a href="userhome.php">Home</a></li>
-  <li><a href="aboutus.php">About Us</a></li>
-  <li><a href="login.php">Place Order</a></li>
-  <!--<li><a href="login.php">Login</a></li>-->
-  <li class="dropdown"><a href="order.php" class="dropbtn">Items</a>
-    <div class="dropdown-content" >
-      <a href="women1.php">Women</a>
-      <a href="men.php">Men</a>
-    </div>
-  </li>
-  <!--<li><a href="register.php">Register</a></li>-->
-  <li><a href="contact.php">Contact Us</a></li>
-  <form align="right" name="form1" method="post" action="logout.php">
-  <label class="logoutLblPos">
-  <input name="submit2" type="submit" id="submit2" value="log out">
-  </label>
-</form>
-   
-</ul>
-<!--<button><a href="logout.php">Logout</a></button>-->
-
 
 <div class="bg">
-<h1><b>  <font color="white" face="courier" size= "6">Select your Orders</font></b></h1><br>
+<h1><b>  <font color="red" face="courier" size= "6">Select your Orders</font></b></h1><br>
 
 <div class= "row">
 
 <div class= "column">
 <img src="images\women\women.jpg" width="400" height="300">&emsp;
-<b><a href="women1.php" font color="#C70039" face="courier" size= "5"><br><font color="white" face="courier" size= "4"><b>Women</b></font>
+<b><a href="women1.php" font color="#C70039" face="courier" size= "5" style="margin-left:10px;"><br><font color="red" face="courier" size= "4"><b>Women</b></font>
 </div>
 <div class= "column">
 <img src="images/men/men.jpg" width="400" height="300">&emsp;
-<b><a href="men.php" font color="#C70039" face="courier" size= "5"><br><font color="white" face="courier" size= "4"><b>Men</b></font>
+<b><a href="men.php" font color="#C70039" face="courier" size= "5"><br><font color="red" face="courier" size= "4"><b>Men</b></font>
 </div>
 
 </div><br>
 </div>
+<?php include 'footer.php'?>
 </body>
 </html>
